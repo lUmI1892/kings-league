97d2b680be19e4c2e9ae3bea1ca5b03430e85d52
lUmI1892 <miguel18592@gmail.com>
2023-01-12 12:12:27 -0400

    rout serveStatic for images

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 
 import { Hono } from 'hono';
 import leaderboard from '../db/leaderboard.json';
+import { serveStatic } from 'hono/serve-static.module';
 
 const app = new Hono();
 
-app.get('/', (ctx)=>{
-	return ctx.json([
+app.get('/', (ctx)=>{([
 		{
 			endpoint: '/leaderboard',
 			description: 'Return the leaderboard'
@@ -16,5 +16,7 @@ app.get('/', (ctx)=>{
 app.get('/leaderboard', (ctx)=>{
 	return ctx.json(leaderboard);
 })
+
+app.get('/static/*', serveStatic({ root: './' }))
 
 export default app;
