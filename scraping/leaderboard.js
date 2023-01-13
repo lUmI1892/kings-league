55e85a5be8ca53cc13d5e72aeb29a049d410d80e
lUmI1892 <miguel18592@gmail.com>
2023-01-13 01:12:38 -0400

    optimize loaderboar for index db

--- scraping/leaderboard.js
+++ scraping/leaderboard.js
@@ -1,11 +1,5 @@
 import * as cheerio from 'cheerio';
-import { writeFile, readFile } from 'node:fs/promises'
-import path from 'node:path';
-
-const DB_PATH = path.join(process.cwd(), './db/');
-const TEAMS = await readFile(`${DB_PATH}/teams.json`, 'utf-8').then(JSON.parse);
-
-/* import TEAMS from '../db/teams.json' assert { type: "json" }; */
+import { TEAMS, writeDBFile, PRESIDENTS } from '../db/index.js';
 
 const URLS = {
     loaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/'
@@ -31,7 +25,15 @@ async function getLoaderBoard(){
         redCard: {selector:'.fs-table-text_9', typeOf: 'number'}
     }
 
-    const getTeamFrom = (name)=> TEAMS.find(team => team.name === name);
+    const getTeamFrom = (name)=> {
+        const {presidentId, ...restOfTeam} = TEAMS.find(team => team.name === name);
+        const president = PRESIDENTS.find(president => president.id === presidentId);
+
+        return {
+            ...restOfTeam,
+            president
+        }
+    }
 
     const cleanText = text => text
     .replace(/\t|\n|\s:/g, '')
@@ -65,4 +67,4 @@ async function getLoaderBoard(){
 }
 
 const leaderboard = await getLoaderBoard();
-await writeFile( `${DB_PATH}/leaderboard.json`, JSON.stringify(leaderboard, null, 2), 'utf-8' );
+writeDBFile('leaderboard', leaderboard);
